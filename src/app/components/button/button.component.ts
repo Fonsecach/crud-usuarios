af4a790bb7dbc884a5f2509f498b8f0fcb8ef07e
Cleiton H. da Fonseca <chdf@proton.me>
2024-03-16 22:46:33 -0300

    Projeto_Crud_Angular

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss'
+})
+export class ButtonComponent {
+
+  @Input() textButton: string = '';
+  @Input() size: string = 'auto';
+
+  constructor() {
+    
+  }
+
+}
